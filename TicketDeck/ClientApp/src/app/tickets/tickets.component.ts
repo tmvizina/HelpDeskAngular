45969d88fa48c5533b52fe483c998e03eea9895a
tmvizina <tmvizina@gmail.com>
2021-07-22 19:36:13 -0400

    Fixed Add button

--- TicketDeck/ClientApp/src/app/tickets/tickets.component.ts
+++ TicketDeck/ClientApp/src/app/tickets/tickets.component.ts
@@ -17,6 +17,8 @@ export class TicketsComponent {
   //array of Tickets
   public tickets: Tickets[] = [];
   closeResult: string;
+  public apiBase: string = "";
+  public http: HttpClient = null;
 
   constructor(private modalService: NgbModal, http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Tickets[]>(baseUrl + 'api/tickets').subscribe(result => {
@@ -24,19 +26,28 @@ export class TicketsComponent {
       console.log(this.tickets);
     }, error => console.error(error));
 
+    this.apiBase = baseUrl;
+    this.http = http;
   }
 
   //Adding a new ticket to the tickets database
+
+
   addTicket(form: NgForm) {
-    let title = form.form.value.title;
-    let description = form.form.value.descrption;
+    console.log(form.form.value.title)
+    let title = form.form.value.title; 
+
+    let description = form.form.value.description;
     let resolved = form.form.value.resolved;
     let solution = form.form.value.solution;
-    let Priotity = form.form.value.priority;
-    let ticket: Tickets = {TicketId: undefined, Title: title, Description: description, Resolved: resolved, Solution: solution, Priotity: Priotity }
-    this.tickets.push(ticket);
-  }
+    let priority = form.form.value.priority;
+    let ticket: Tickets = { TicketId: 0, Title: title, Description: description, Resolved: resolved, Solution: solution, priority: priority }
+    this.http.post<Tickets>(this.apiBase + 'api/Tickets', ticket).subscribe(result => {
+      console.log(result)
 
+      this.tickets.push(result);
+    });
+  }
 
 
   open(content) {
