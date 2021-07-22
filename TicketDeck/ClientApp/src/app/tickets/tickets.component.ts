8a9d6d79a7d6cfc71b059492c4e3519cb6dde690
jerome <jeromebrown97@gmail.com>
2021-07-22 16:52:42 -0500

    working on ticket functionality

--- TicketDeck/ClientApp/src/app/tickets/tickets.component.ts
+++ TicketDeck/ClientApp/src/app/tickets/tickets.component.ts
@@ -4,26 +4,46 @@ import { HelpDeskService } from '../help-desk.service';
 import { HttpClient } from '@angular/common/http';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
+  styleUrls: ['./tickets.component.css'],
   providers: [HttpClient]
 })
 /** Tickets component*/
 export class TicketsComponent {
-  title = 'appBootstrap';
 
-  //array of Tickets
   public tickets: Tickets[] = [];
+  public route: Router;
+  public http: HttpClient = null;
+  public apiBase: string = "";
+  public postTickets: HttpClient = null;
+  title = 'appBootstrap';
   closeResult: string;
 
+
   constructor(private modalService: NgbModal, http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.apiBase = baseUrl;
+    this.http = http;
+
+
     http.get<Tickets[]>(baseUrl + 'api/tickets').subscribe(result => {
       this.tickets = result;
       console.log(this.tickets);
     }, error => console.error(error));
 
+  }
+
+
+
+  //will call API and update the display
+  displayTicket(http: HttpClient) {
+    http.get<Tickets[]>(this.apiBase + 'api/tickets').subscribe(result => {
+      this.tickets = result;
+      console.log(this.tickets);
+    }, error => console.error(error));
   }
 
   //Adding a new ticket to the tickets database
@@ -33,11 +53,19 @@ export class TicketsComponent {
     let resolved = form.form.value.resolved;
     let solution = form.form.value.solution;
     let Priotity = form.form.value.priority;
-    let ticket: Tickets = {TicketId: undefined, Title: title, Description: description, Resolved: resolved, Solution: solution, Priotity: Priotity }
+    this.http.post<Tickets>(this.apiBase + 'api/tickets?title=' + title, {}).subscribe(result => {
+    console.log(result)
+    let ticket: Tickets = { TicketId: undefined, Title: title, Description: description, Resolved: resolved, Solution: solution, Priotity: Priotity };
     this.tickets.push(ticket);
+    });
   }
 
 
+
+  buttonSubmit(form: NgForm) {
+    this.addTicket(form);
+    this.displayTicket(this.http);
+  }
 
   open(content) {
 
