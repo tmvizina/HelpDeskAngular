20a621f6b2840caad8e3ef19b41be795ac697351
jerome <jeromebrown97@gmail.com>
2021-07-23 13:29:59 -0500

    working on tickets functionality

---
+++ TicketDeck/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace TicketDeck.Models
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            Bookmarks = new HashSet<Bookmarks>();
+        }
+
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Bookmarks> Bookmarks { get; set; }
+    }
+}
