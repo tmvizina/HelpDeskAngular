816f026b4170f40b92d119b09ab523f70b8bcf68
Kate Datema <k8datema@gmail.com>
2021-07-23 22:13:28 -0400

    GOOD BRANCH, updating to overwrite main

--- TicketDeck/Models/HelpDeskContext.cs
+++ TicketDeck/Models/HelpDeskContext.cs
@@ -27,7 +27,7 @@ namespace TicketDeck.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
                 optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=HelpDesk;Trusted_Connection=True;");
             }
         }
